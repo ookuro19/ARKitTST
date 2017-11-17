77b82a0b235a47c44ad3fca0034361999a3cb956
NeunZehn <nzdream@163.com>
2017-11-17 23:38:25 +0800

    Rewrite the ChangeScene.cs

---
+++ Assets/Attempts/ChangeSceneTST/GlobalForChangeScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GlobalForChangeScene {
+
+	public static Vector3 GlobalPosition{ get; private set;}
+	private static bool IsGlobalPosition = false;
+
+	public static void SetGlobalPosition(Vector3 tPosition)
+	{
+		GlobalPosition = tPosition;
+	}
+
+}
